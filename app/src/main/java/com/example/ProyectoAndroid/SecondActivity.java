3b7fa31d4d39e342d97098980c3016c252effe99
jesusrenne <jesussrenne@yahoo.es>
2021-10-30 08:42:17 -0500

    Activación del botón volver (Actividad 2 a Menú Principal)

--- app/src/main/java/com/example/ProyectoAndroid/SecondActivity.java
+++ app/src/main/java/com/example/ProyectoAndroid/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,8 +14,25 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        //Crear la barra para el menú
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Llamar al método que recibe los parámetros
         getParametros();
+    }
+    //Método para destruir la aplicación
+    public void onBackPressed(){
+        finish();
+
+    }
+    //Método para crear las opciones en el menú
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id = menuItem.getItemId();
+        if(id == android.R.id.home){
+            onBackPressed();
+            return  true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+
     }
 
     //Crear un nuevo método para recibir los parámetros
